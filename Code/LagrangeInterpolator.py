ecd025350194fba2bbfb2e88f07200105b4e2a11
Artur Prokopenko <arturprokopenko01@gmail.com>
2021-12-11 13:58:59 +0300

    lagr

---
+++ Code/LagrangeInterpolator.py
@@ -0,0 +1,6 @@
+class LagrangeInterpolator:
+    def __init__(self, lib):
+        self.lib = lib
+
+    def lagrange_interpolate(self, x_values, y_values, initial_count, start, end, dots_count, x_buffer, y_buffer):
+        pass
